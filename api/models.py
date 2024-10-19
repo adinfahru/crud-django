fad371fecd8eb78263c2c7b64e4e7553baba660f
adinfahru <mohamad.fahrudin18@gmail.com>
2024-10-19 10:54:08 +0700

    initial commit

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
